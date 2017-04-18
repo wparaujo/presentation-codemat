c0827c0a54b49af6efde31042b50370f91dfa5d2
Miguel Nery <nery.mik@gmail.com>
2017-04-17 23:26:34 -0300

    category attribute removed from dojo

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -9,17 +9,17 @@ class DojosController < ApplicationController
 	# create facade
 	def create
 		# user = User.find current_user.id
-		@dojo = Dojo.new(dojo_params) 
+		@dojo = Dojo.new(dojo_params)
 		current_user.dojos << @dojo
 
 		if user.save
-			
+
 		else
 		end
 	end
 
 	def edit
-		
+
 	end
 
 	def update
@@ -28,8 +28,8 @@ class DojosController < ApplicationController
 	def destroy
 	end
 
-	private 
+	private
 	def dojo_params
-		params.require(:dojo).permit(:user_id, :title, :category)
+		params.require(:dojo).permit(:user_id, :title)
 	end
 end
--- db/migrate/20170409180152_create_dojos.rb
+++ db/migrate/20170409180152_create_dojos.rb
@@ -3,7 +3,6 @@ class CreateDojos < ActiveRecord::Migration[5.1]
     create_table :dojos do |t|
       t.belongs_to :user, index: true
       t.string :title
-      t.string :category
       t.timestamps
     end
   end
