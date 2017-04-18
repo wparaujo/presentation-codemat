8a386f628cf9b5a06827a5f51e68160b9c96acb5
Miguel Nery <nery.mik@gmail.com>
2017-04-17 23:30:30 -0300

    attributes added to dojo creation

--- app/controllers/dojo_creator_controller.rb
+++ app/controllers/dojo_creator_controller.rb
@@ -4,11 +4,15 @@ class DojoCreatorController < ApplicationController
     randori: Randori
   }
 
-  def self.dojoFactory(type)
-    if DojoTypes[type]
-      return DojoTypes[type].new
+  def self.dojoFactory(category, title)
+    if DojoTypes[category]
+       newDojo = DojoTypes[category].new
+       newDojo.user = current_user.id
+       newDojo.title = title
+
+       return newDojo
     else
-      raise "Undefined Dojo type"
+      raise "Undefined Dojo category"
     end
 
   end
