971259553caccbb47d0565d51b53b6c9708e6326
Luan Guimarães <livreluan@gmail.com>
2017-04-19 03:33:45 -0300

    Associating themes and uses with Observer pattern

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,8 @@
 class Theme < ApplicationRecord
   has_many :dojos
+  has_and_belongs_to_many :observers,
+                          class_name: "User",
+                          join_table: "notifiers_observers"
   after_update :notify_observers
 
   include Notifier
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
 	has_secure_password
 
 	has_many :dojos
+  has_and_belongs_to_many :theme_notifiers,
+                          class_name: "Theme",
+                          join_table: "notifiers_observers"
 	accepts_nested_attributes_for :dojos
 end
